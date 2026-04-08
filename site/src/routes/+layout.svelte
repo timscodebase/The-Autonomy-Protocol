26aa028dd0090c2c757420e028f37374ea345f71
Tim Smith <tim.smith.hdg@gmail.com>
2026-04-08 19:41:00 +0000

    site init

---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import "./global.css"
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="layout-wrapper">
+	<header>
+		Header
+	</header>
+
+	<main>
+		{@render children()}
+	</main>
+
+	<footer>
+		<p>The Autonomy Project &copy; {new Date().getFullYear()}</p>
+	</footer>
+</div>
+
+<style>
+	.layout-wrapper {
+		min-height: 100svh;
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+	}
+</style>
