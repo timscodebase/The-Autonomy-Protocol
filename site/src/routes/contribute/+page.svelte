e3209df2eb17ee7a80c6d633cf2a70957dd0370d
Tim Smith <tim.smith.hdg@gmail.com>
2026-04-10 11:19:10 -0700

    Update +page.svelte

--- site/src/routes/contribute/+page.svelte
+++ site/src/routes/contribute/+page.svelte
@@ -1,1 +1,19 @@
+<script lang="ts">
+    import HeroImage from '$images/img-4.jpg?enhanced&w=400;800;1600;2400';
+</script>
 
+<section class="hero">
+    <enhanced:img
+		src={HeroImage}
+		sizes="(min-width: 800px) 800px, 100vw"
+		alt="" 
+		aria-hidden="true"
+		class="hero-image"
+	/>
+	<div>
+		<h1>We Need Contributers</h1>
+		<p>
+      Would you like to contribute to this project?  We need teachers and administrators with experience to help build lesson plans, homework, artwork and other content.  You could help shap the future of our students and future leaders!
+		</p>
+	</div>
+</section>
