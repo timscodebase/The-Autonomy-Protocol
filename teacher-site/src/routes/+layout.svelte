91a506c2459a4a414b24e547dfa449128ffffa89
Tim Smith <tim.smith.hdg@gmail.com>
2026-04-18 11:56:50 -0700

    Update +layout.svelte

--- teacher-site/src/routes/+layout.svelte
+++ teacher-site/src/routes/+layout.svelte
@@ -54,6 +54,7 @@
 		margin: var(--gap-2) auto;
 		min-height: 100svh;
 		display: grid;
+        padding: var(--gap-1);
 		padding: clamp(1rem, 2.75svw, 2rem);
 		grid-template-rows: auto 1fr auto;
 		background: var(--surface-4);
